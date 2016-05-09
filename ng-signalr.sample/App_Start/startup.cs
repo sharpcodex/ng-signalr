42c293fffbbe90601b999e5241e56fd4aecb0a4d
sharpcodex <sharpcodex@gmail.com>
2016-05-09 14:34:55 +0300

    configure signalr in the sample project

---
+++ ng-signalr.sample/App_Start/startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ng_signalr.sample.App_Start.Startup))]
+namespace ng_signalr.sample.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfiguration = new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            };
+
+            app.MapSignalR("/signalr", hubConfiguration);
+        }
+    }
+}
